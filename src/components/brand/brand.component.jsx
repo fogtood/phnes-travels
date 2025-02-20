e1b18519a4df2ab430735f1c866520591c491181
Ayokayzy <theayokayzy1@gmail.com>
2025-02-20 14:19:15 +0100

    update

--- src/components/brand/brand.component.jsx
+++ src/components/brand/brand.component.jsx
@@ -4,7 +4,7 @@ import brand from "@/assets/images/brand.png";
 const Brand = () => {
   return (
     <NavLink to="/" className="inline-flex flex-col items-center justify-start">
-      <img src={brand} alt="brand" className="h-16" />
+      <img src={brand} alt="brand" className="h-20" />
       <p className="text-sm">Study and Fly</p>
     </NavLink>
   );
